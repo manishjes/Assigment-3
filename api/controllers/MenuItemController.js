1028082ac83dc5dcf367d1bd947fc6005f8db6bd
manishjes <manishj@zignuts.com>
2023-03-21 10:07:33 +0530

    update

--- api/controllers/MenuItemController.js
+++ api/controllers/MenuItemController.js
@@ -14,7 +14,7 @@ module.exports = {
         
       });
 
-      //item name and category validation
+      //item name validation
       if (items.length != 0) {
         return res.status(404).json({ message: sails.__("item.exsits", { lang: lang }) });
       }
@@ -97,34 +97,19 @@ module.exports = {
         itemname: itemname,
         
       });
-      const itemorders = await MenuItem.find({
-        displayOrder: displayOrder
-      });
+      
 
-      // item name and category validation
+      // item name validation
       if (items.length != 0) {
         return res.status(404).json({ message: "item name already exists" });
       }
 
-      // displayOrder and category validation
-      if (itemorders.length != 0) {
-        return res
-          .status(404)
-          .json({ message: `item order ${displayOrder} already exists` });
-      }
+      
 
       // update the item
       const item = await MenuItem.updateOne({ id: id }).set(
-        {
-        itemname: itemname,
-        description: description,
-        price: price,
-        category: category,
-        image: image,
-        displayOrder: displayOrder,
-        updatedAt: new Date(),
-      }
-      );
+        req.body);
+      
 
       // return the updated item
       return res.status(200).json({ message: sails.__("item.update", { lang: lang }), item: item });
